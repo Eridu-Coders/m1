db5849dc9d946f731bd3abea6a2e39fe26361571
fi11222 <nicolas.reimen@gmail.com>
2024-02-24 13:55:32 +0530

    QWidgets introduction and tree restructure

--- main.cpp
+++ main.cpp
@@ -1,99 +1,11 @@
-#include <QDebug>
+#include <QApplication>
 
-#include <gtest/gtest.h>
-
-#include "m1_env.h"
-#include "m1_store.h"
-#include "m1_lv2_item.h"
-
-#include <boost/filesystem.hpp>
-
-#include <string>
+#include "m1_main_window.h"
 
 int main(int argc, char *argv[])
 {
-    M1Store::M1Env::init();
-    M1Store::Storage::storeSetUp();
-
-    qDebug() << "Main start";
-
-    qCDebug(g_cat_store) << QString("ROOT_SPECIAL_ID = %1").arg(M1Store::ROOT_SPECIAL_ID);
-    qCDebug(g_cat_store) << QString("HOME_SPECIAL_ID = %1").arg(M1Store::HOME_SPECIAL_ID);
-    qCDebug(g_cat_store) << QString("TEXT_SPECIAL_ID = %1").arg(M1Store::TEXT_SPECIAL_ID);
-    qCDebug(g_cat_store) << QString("T_WORD_SPECIAL_ID = %1").arg(M1Store::T_WORD_SPECIAL_ID);
-
-    if(true){
-        qDebug() << "opening file";
-        QString l_path("../Enoch-words.txt");
-        int l_count = 0;
-        if(boost::filesystem::exists(l_path.toStdString())){
-            boost::filesystem::ifstream fileHandler(l_path.toStdString());
-            std::string line;
-            while(getline(fileHandler, line)){
-                /*
-                QLoggingCategory::setFilterRules("*.debug=false\n"
-                                                 "dump.debug=true");
-                M1Store::ItemWrapper::recurGraphStart(M1Store::Storage::getSpecial("ROOT_")->itemId());
-                QLoggingCategory::setFilterRules("*.debug=true");
-                */
-                qDebug() << line;
-                M1Store::ItemWrapper* l_word = M1Store::ItemWrapper::mbd_getNew(
-                    // category & attributes
-                    M1Store::FULL_VERTEX,
-                    // type
-                    M1Store::ItemType(
-                        M1Store::T_WORD_SPECIAL_ID,
-                        M1Store::G_VOID_SI_ID,
-                        M1Store::G_VOID_SI_ID,
-                        M1Store::G_VOID_SI_ID),
-                    // label
-                    line.c_str()
-                );
-                delete l_word;
-                // if(l_count == 3) break;
-                l_count += 1;
-            }
-        }
-        else
-            qDebug() << "can't open";
-    }
-
-    qDebug() << "Main end";
-
-    /*
-    M1Store::Storage::storeString(QString("Start [%1]").arg(QDateTime::currentDateTime().toString("dd/MM/yyyy hh:mm:ss")));
-
-    qDebug() << "sizeof(M1Store::ItemType):" << sizeof(M1Store::ItemType);
-    qDebug() << "sizeof(M1Store::Item_lv0):" << sizeof(M1Store::Item_lv0);
-    qDebug() << "sizeof(M1Store::ItemWrapper):" << sizeof(M1Store::ItemWrapper);
-    qDebug() << "sizeof(M1Store::ItemWrapperFullVertex):" << sizeof(M1Store::ItemWrapperFullVertex);
-
-    char l_buf0[128];
-    char l_buf1[128];
-    M1Store::ItemWrapper* l_wi0 = M1Store::ItemWrapper::instantiateFromMMap((void*)l_buf0, 0, M1Store::FULL_VERTEX, M1Store::ItemType());
-    M1Store::ItemWrapper* l_wi1 = M1Store::ItemWrapper::instantiateFromMMap((void*)l_buf1, 0, M1Store::FULL_EDGE, M1Store::ItemType());
-
-    l_wi0->setText("toto");
-    l_wi1->setText("tutu");
-
-    qDebug() << l_wi0->dbgString().toUtf8().constData();
-    qDebug() << l_wi1->dbgString().toUtf8().constData();
-
-    delete l_wi0;
-    delete l_wi1;
-
-    unsigned long p0 = (unsigned long)M1Store::Storage::getItemSlotPointer(0);
-    unsigned long p1 = (unsigned long)M1Store::Storage::getItemSlotPointer(1);
-
-    qDebug() << QString("p0: 0x%01").arg(p0, 16, 16);
-    qDebug() << QString("p1: 0x%01").arg(p1, 16, 16);
-    qDebug() << "Difference: " << (p1 - p0);
-
-    M1Store::Storage::storeString(QString("End [%1]").arg(QDateTime::currentDateTime().toString("dd/MM/yyyy hh:mm:ss")));
-    */
-
-    M1Store::Storage::storeShutDown();
-    M1Store::M1Env::close();
-
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
---
+++ main_test_store_lv1.cpp
@@ -0,0 +1,29 @@
+#include <QDebug>
+
+#include <gtest/gtest.h>
+
+#include "m1_env.h"
+#include "m1_store.h"
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    M1Store::M1Env::init();
+    M1Store::Storage::storeSetUp();
+
+    M1Store::Storage::storeString(QString("Dummy Start [%1]").arg(QDateTime::currentDateTime().toString("dd/MM/yyyy hh:mm:ss")));
+
+    int l_ret = RUN_ALL_TESTS();
+    qDebug() << "After Tests";
+
+    qDebug() << "sizeof(M1Store::ItemType):" << sizeof(M1Store::ItemType);
+    qDebug() << "sizeof(M1Store::Item):" << sizeof(M1Store::Item_lv0);
+
+    M1Store::Storage::storeString(QString("Dummy End [%1]").arg(QDateTime::currentDateTime().toString("dd/MM/yyyy hh:mm:ss")));
+
+    M1Store::Storage::storeShutDown();
+    M1Store::M1Env::close();
+
+    return l_ret;
+    //return 0;
+}
